f47ff88c1dec2fa47a754f5d65d826756f5bff8b
anurag <anuragrawat92003@gmail.com>
2024-07-31 17:13:55 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from './components/Header'
 import Meme from './components/Meme'
 import './App.css'
+import memesData from './components/memesData'
 
 export default function App(){
+  const memeImage=memesData.map(item=>{
+    return(
+      console.log(item.url)
+    )
+  })
   return(
     <>
     <Header />
-    <Meme />
+    <Meme onclick={memeImage}/>
     </>
   )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,18 @@
+import memesData from "./memesData"
 export default function Meme(){
+    function getMemeData(){
+        const data=memesData.data.memes;
+        const random =Math.floor(Math.random*data.length());
+        const url = data[random].url;
+        console.log(url);
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form-input"  placeholder="Top text" />
                 <input type="text" className="form-input" placeholder="Bottom text"  />
-                <button className="form-button">Get a new meme image 🖼</button>
-            </form>
+                <button className="form-button" onClick={getMemeData}>Get a new meme image 🖼</button>
+            </div>
         </main>
     )
 } 
