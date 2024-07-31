2aaa59821453bf628bc6ea6faf94f5f709e6e28a
anurag <anuragrawat92003@gmail.com>
2024-07-31 16:32:27 +0530

    Created top-bar , form inputs ,button and provided the meme data.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import trollface from '../assets/TrollFace.svg'
+export default function Header(){
+    return(
+        <header className='header'> 
+            <img src={trollface} className='nav-logo' />
+            <h3 className='header-title'>Meme Generator</h3>
+           <h5 className='header-content'>React Course -Project 3</h5>
+        </header>
+    )
+}
