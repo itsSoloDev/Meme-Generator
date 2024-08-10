1f2d9eea0406fc94c8e4735ab4a01ff3f18bb721
anurag <anuragrawat92003@gmail.com>
2024-08-10 14:49:12 +0530

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
         <header className='header'> 
             <img src={trollface} className='nav-logo' />
             <h3 className='header-title'>Meme Generator</h3>
-           <h5 className='header-content'>React Course -Project 3</h5>
+           {/* <h5 className='header-content'>React Course -Project 3</h5>  */}
         </header>
     )
 }
